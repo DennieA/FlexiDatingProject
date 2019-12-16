2bf1688bc4724543b99dc67e3904bc9dcd7bfc93
Dennie Aertsens <Dennie.Aertsens@ad.core.vdabantwerpen.intra>
2019-12-16 15:34:03 +0100

    Lovecoins kopen toegevoegd

--- Website/js/WijzigProfielgegevens.js
+++ Website/js/WijzigProfielgegevens.js
@@ -73,8 +73,6 @@ function GebruikersGegevens (el){
         document.getElementById("oogkleur").value = el.oogkleur;
         document.getElementById("grootte").value = el.grootte;
         document.getElementById("gewicht").value = el.gewicht;
-
-        document.getElementById("lovecoins").value = el.lovecoins;
         
 }
 
@@ -104,8 +102,7 @@ function submit() {
     profiel.oogkleur = oogkleur.options[oogkleur.selectedIndex].innerHTML;
     profiel.grootte = document.getElementById("grootte").value;
     profiel.gewicht = document.getElementById("gewicht").value;
-    profiel.lovecoins = document.getElementById("lovecoins").value;
-    if (profiel.familienaam === "" || profiel.voornaam === "" || profiel.nickname === "" || profiel.geboortedatum === "" || profiel.email === "" || profiel.foto === "" || profiel.beroep === "" || profiel.lovecoins === "" ||profiel.sexe === "" || profiel.grootte === "" || profiel.gewicht === "" ) 
+    if (profiel.familienaam === "" || profiel.voornaam === "" || profiel.nickname === "" || profiel.geboortedatum === "" || profiel.email === "" || profiel.foto === "" || profiel.beroep === "" ||profiel.sexe === "" || profiel.grootte === "" || profiel.gewicht === "" ) 
         window.alert(" Gelieve alle velden in te vullen! ");
             
             let urlUpdate = 'https://scrumserver.tenobe.org/scrum/api/profiel/update.php';
