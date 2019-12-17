1525753b2b3ac0dd64913cd9e26fbfad24293ccb
Yannis Decoster <yannis_decoster@hotmail.com>
2019-12-17 10:49:49 +0100

    update mainjs with link to profiles

--- Website/js/main.js
+++ Website/js/main.js
@@ -481,7 +481,7 @@ function GebruikersGegevens(data) {
         sterrenbeeldCell.innerHTML = `<img src="images/icons/${getZodiacSign(el.geboortedatum)}.png" title = ${getZodiacSign(el.geboortedatum)}>`;
 
         const nicknameCell = tableRowBody.insertCell();
-        nicknameCell.innerHTML = `<a onclick='passNickname(${el.nickname})'>${el.nickname}</a>`;
+        nicknameCell.innerHTML = `<a onclick='passNickname("${el.nickname}")'>${el.nickname}</a>`;
         nicknameCell.id = el.id;
 
         const beroepCell = tableRowBody.insertCell();
@@ -626,6 +626,7 @@ function getZodiacSign(date) {
 }
 
 function passNickname(nickname){
+    console.log(nickname);
     sessionStorage.setItem("selectedNickname", nickname);
     window.open("matchProfile.html","_self")
 }
